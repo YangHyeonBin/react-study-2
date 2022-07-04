b7492b7cc9f3b7ae18a465dd5425d080223e019d
ekil <idgusqls0506@naver.com>
2022-07-04 16:28:21 +0900

    dynamical styling with css module

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,38 +1,40 @@
-// JSX 안 써서 React import X
-import styled from 'styled-components';
+import React from 'react';
 
-const Button = styled.button`
-  width: 100%; /* 작은 화면에선 큰 버튼, 이걸 디폴트로 */
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+import styles from './Button.module.css'; // CSS module // styles 작명은 자유, 객체임
+// import styled from 'styled-components';
 
-  @media (min-width: 768px) {
-    width: auto;
-  } /* 미디어 쿼리, 큰 화면엔 작은 버튼 */
+// const Button = styled.button`
+//   width: 100%; /* 작은 화면에선 큰 버튼, 이걸 디폴트로 */
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  &:focus {
-    outline: none;
-  }
+//   @media (min-width: 768px) {
+//     width: auto;
+//   } /* 미디어 쿼리, 큰 화면엔 작은 버튼 */
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`; // taged template literal
+//   &:focus {
+//     outline: none;
+//   }
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `; // taged template literal
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
